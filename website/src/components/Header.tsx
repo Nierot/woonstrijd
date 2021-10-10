62b07bf3d14bc6055f40d1362d542de2b724ff09
Nierot <git@nierot.com>
2021-10-10 21:16:28 +0200

    ✨ Added hamburger menu

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Menu from "./Menu";
 
 const openMenu = () => {
 
@@ -6,17 +7,12 @@ const openMenu = () => {
 const Header: React.FC = () =>
 <header className="grid grid-cols-6 bg-header h-16">
     <p className="col-span-5">
-        haha leuke header
+        {/* haha leuke header */}
     </p>
 
     <div className="flex justify-end py-2 px-2">
         {/* https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/ */}
-        {/* https://www.npmjs.com/package/react-hamburger-menu */}
-        <button onClick={openMenu}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-        </button>
+        <Menu />
     </div>
 </header>
 
--- website/src/components/Menu.tsx
+++ website/src/components/Menu.tsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router-dom"
+import HamburgerMenuIcon from 'react-hamburger-menu'
+import { slide as HamburgerMenu } from 'react-burger-menu'
+import { useState } from "react";
+import './HamburgerMenu.css'
 
-const Menu: React.FC = () => 
-<div>
-    <MenuButton to="over-ons" text="Over ons" />
-    <MenuButton to="manifest" text="Manifest" />
-    <MenuButton to="nieuws" text="Nieuws" />
-    <MenuButton to="materiaal" text="Materiaal" />
 
-    {/* TODO doneer knop onderaan zetten en anders maken */}
-    <MenuButton to="doneer" text="Doneer"/>
+const Menu: React.FC = () => {
+    const [open, setOpen] = useState(false)
 
-</div>
+    return <div className="pt-2 pr-2">
+        <HamburgerMenu right isOpen={open} customBurgerIcon={false}>
+            <MenuButton to="over-ons" text="Over ons" />
+            <MenuButton to="manifest" text="Manifest" />
+            <MenuButton to="nieuws" text="Nieuws" />
+            <MenuButton to="materiaal" text="Materiaal" />
+
+            {/* TODO doneer knop onderaan zetten en anders maken  */}
+            <MenuButton to="doneer" text="Doneer"/>
+        </HamburgerMenu>
+        <HamburgerMenuIcon
+            isOpen={open}
+            menuClicked={() => setOpen(!open)}
+            color="#FFF"
+        />
+    </div>
+
+}
 
 type MenuButtonProps = {
     to: string
@@ -18,7 +33,7 @@ type MenuButtonProps = {
 }
 
 const MenuButton = (props: MenuButtonProps) => 
-<div>
+<div className="text-white">
     <Link to={props.to}>
         {props.text}
     </Link>
